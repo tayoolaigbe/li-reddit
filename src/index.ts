2dbe0d7efbbd2498e305911144d74d394e1923aa
Eyitayo Olaigbe <48358258+tayoolaigbe@users.noreply.github.com>
2021-06-25 20:31:18 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import { HelloResolver } from './migrations/resolvers/hello';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
 	const orm = await MikroORM.init(microConfig);
@@ -15,9 +16,10 @@ const main = async () => {
 
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
-			resolvers: [HelloResolver],
+			resolvers: [HelloResolver, PostResolver],
 			validate: false,
 		}),
+		context: () => ({ em: orm.em }),
 	});
 
 	apolloServer.applyMiddleware({ app });
