65775a558f4f0eff880cb7dac53e191c63e0ac5b
Eyitayo Olaigbe <48358258+tayoolaigbe@users.noreply.github.com>
2021-06-25 19:19:12 +0100

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './migrations/resolvers/hello';
+
+const main = async () => {
+	const orm = await MikroORM.init(microConfig);
+	orm.getMigrator().up();
+
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: false,
+		}),
+	});
+
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(4000, () => {
+		console.log(`Server is listening at port 4000`);
+	});
+};
+
+main().catch(err => {
+	console.error(err);
+});
